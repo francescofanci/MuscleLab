fc335c75476ffc678bd926f7129cc9200091ffdc
Lor3nzo03 <lorenzochieppa03@gmail.com>
2025-05-07 18:49:06 +0200

    piccole modifiche

--- js/generator.js
+++ js/generator.js
@@ -3,10 +3,11 @@
     const tbody = document.querySelector("#workout-table tbody");
     const row = document.createElement("tr");
     row.innerHTML = `
-      <td><input type="text" placeholder="Es. Schiena"></td>
-      <td><input type="text" placeholder="Es. Lat Machine"></td>
-      <td><input type="number" min="1" placeholder="3"></td>
-      <td><input type="number" min="1" placeholder="10"></td>
+      <td><input type="text"></td>
+      <td><input type="text"></td>
+      <td><input type="number" min="1"></td>
+      <td><input type="number" min="1"></td>
+      <td><input type="number" min="1"></td>
       <td><button onclick="rimuoviRiga(this)">🗑️</button></td>
     `;
     tbody.appendChild(row);
